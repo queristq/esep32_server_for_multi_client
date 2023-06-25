300ec8f84e24e430a3459464cfeec43d32a8498b
Youngmok Yun <my last name and first name@gmail.com>
2023-06-24 22:43:44 -0500

    correcting the folder

---
+++ python/client.py
@@ -0,0 +1,23 @@
+# Import socket module
+import socket            
+import time
+
+def delay_5_seconds():
+  time.sleep(5)
+
+# Create a socket object
+s = socket.socket()        
+ 
+# Define the port on which you want to connect
+host = '192.168.1.9'
+port = 80               
+
+for i in range(0,10):     
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((host,port))
+        s.sendall(b"Hello, world")
+        data = s.recv(1024)
+        print(f"Received {data!r}")
+        s.close()
+
+
